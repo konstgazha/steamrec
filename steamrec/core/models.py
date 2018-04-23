e5fe6f3ca2e3edbd3b16a8b7feaa597e17818af0
Konstantin Gazha <konstgazha@yandex.ru>
2018-04-23 16:43:23 +0500

    Update the Game model. Add the Tag and GameTag models

--- steamrec/core/models.py
+++ steamrec/core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Game(models.Model):
     appid = models.IntegerField(unique=True)
     name = models.CharField(max_length=128)
+    app_type = models.ForeignKey(AppType)
     release_date = models.DateField(blank=True, null=True)
     header_image_url = models.URLField(blank=True, null=True)
     developer = models.CharField(max_length=128)
@@ -14,6 +15,22 @@ class Game(models.Model):
     total_review_count = models.IntegerField(blank=True, null=True)
     recent_review_score = models.IntegerField(blank=True, null=True)
     total_review_score = models.IntegerField(blank=True, null=True)
+    date_added = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now_add=True, auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class AppType(models.Model):
+    name = models.CharField(max_length=128)
+    display_name = models.CharField(max_length=128)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class GameTag(models.Model):
+    game = models.ForeignKey(Game)
+    tag = models.ForeignKey(Tag)
