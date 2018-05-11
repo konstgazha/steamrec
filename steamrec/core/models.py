cdf1251afaf3dc17a12abd55347772648add1b73
Konstantin Gazha <konstgazha@yandex.ru>
2018-05-11 14:22:57 +0500

    Change models name. Fix date_modified
    Game --> App

--- steamrec/core/models.py
+++ steamrec/core/models.py
@@ -6,7 +6,7 @@ class AppType(models.Model):
     display_name = models.CharField(max_length=128)
 
 
-class Game(models.Model):
+class App(models.Model):
     appid = models.IntegerField(unique=True)
     name = models.CharField(max_length=128)
     app_type = models.ForeignKey(AppType, on_delete=models.CASCADE)
@@ -21,7 +21,7 @@ class Game(models.Model):
     recent_review_score = models.IntegerField(blank=True, null=True)
     total_review_score = models.IntegerField(blank=True, null=True)
     date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now_add=True, auto_now=True)
+    date_modified = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -31,20 +31,20 @@ class Tag(models.Model):
     name = models.CharField(max_length=128)
 
 
-class GameTag(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+class AppTag(models.Model):
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 
 class Player(models.Model):
     steamid = models.CharField(max_length=128, unique=True)
-    game_count = models.IntegerField(blank=True, null=True)
+    app_count = models.IntegerField(blank=True, null=True)
     public = models.BooleanField(default=True)
     avatar = models.CharField(max_length=128)
     avatarmedium = models.CharField(max_length=128)
     avatarfull = models.CharField(max_length=128)
     date_added = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now_add=True, auto_now=True)
+    date_modified = models.DateField(auto_now=True)
 
     def __str__(self):
         return str(self.steamid)
